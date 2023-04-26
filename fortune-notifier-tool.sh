b75c19bc95b3ad07837086c7148a8b908cc3f9f7
Sergey Rogov <rogovsergey2001@gmail.com>
2023-04-26 08:41:52 +0300

    The very first, perhaps slightly non-functional version of the notification app.

--- fortune-notifier-tool.sh
+++ fortune-notifier-tool.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo hello, world
+tty_str=$(tty | grep tty)
+len=${#tty_str}
+msg=$(/usr/bin/fortune)
+
+if [ $len -gt 0 ]; then
+    echo
+    echo $msg
+    echo
+else
+    notify-send -w "$msg" --icon=dialog-information --category=im
+fi
