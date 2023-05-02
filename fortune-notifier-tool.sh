c3fc4867dcfa405c7664668d06a0707f26212a9c
Sergey Rogov <rogovsergey2001@gmail.com>
2023-05-02 14:47:52 +0300

    nothing

--- fortune-notifier-tool.sh
+++ fortune-notifier-tool.sh
@@ -3,11 +3,7 @@ tty_str=$(tty | grep tty)
 len=${#tty_str}
 msg=$(/usr/bin/fortune)
 
-if [ $len -gt 0 ]; then
-    echo
-    echo $msg
-    echo
-else
+if [ $len -eq 0 ]; then
     sleep 5s
     notify-send -w "$msg" --icon=dialog-information --category=im
 fi
