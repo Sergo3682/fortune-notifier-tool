8db6adff0406d521f7e37ea1a6b54995103167d1
Sergey Rogov <rogovsergey2001@gmail.com>
2023-05-29 14:44:29 +0300

    generating .desktop file at autorun dir

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 name=$"fortune-notifier-tool"
-wrk_dir=$"$HOME/test_dir/"
+wrk_dir=$"$HOME/.$name/"
 script=$"$name.sh"
 if [ -d $wrk_dir ]; then
     echo $wrk_dir is alredy exists
@@ -11,3 +11,14 @@ else
 fi
 cp $script $wrk_dir$script
 chmod 744 $wrk_dir$script
+
+autostart_dir=$"$HOME/.config/autostart/"
+array=(
+"Version=1.0" "Type=Application" "Name=$name" "Comment=" 
+"Exec=$wrk_dir$script" "Icon=" "Path=" "Terminal=false" "StartupNotify=false"
+)
+echo [Desktop Entry] > $autostart_dir$name.desktop
+for text in "${array[@]}"
+do
+echo $text >> $autostart_dir$name.desktop
+done
