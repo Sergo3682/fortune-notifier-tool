34b860ebe9456d9f4b2880cdb2fe56b9945d99db
Sergey Rogov <rogovsergey2001@gmail.com>
2023-05-29 19:45:54 +0300

    removed if..fi. cause bad working on different machines

--- fortune-notifier-tool.sh
+++ fortune-notifier-tool.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-tty_str=$(tty | grep tty)
-len=${#tty_str}
 msg=$($(which fortune))
-
-if [ $len -eq 0 ]; then
-    notify-send -w --icon=dialog-information --category=im "$msg"
-fi
+notify-send --icon=dialog-information --category=im "$msg"
