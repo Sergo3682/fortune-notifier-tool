d7ab5de1963b37d2598183afcd584a6572fc227e
Sergo3682 <97442751+Sergo3682@users.noreply.github.com>
2023-05-31 12:09:58 +0300

    added a check for the condition of using the -w option

--- fortune-notifier-tool.sh
+++ fortune-notifier-tool.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 msg=$($(which fortune))
-notify-send --icon=dialog-information --category=im "$msg"
+man=$(man notify-send)
+regex_res=$(grep -E "\-\w, \-\-\wait" <<< $man)
+len_match=${#regex_res}
+
+if [ "$len_match" -gt 0 ]; then
+    notify-send -w --icon=dialog-information "$msg"
+else
+    notify-send --icon=dialog-information "$msg"
+fi
